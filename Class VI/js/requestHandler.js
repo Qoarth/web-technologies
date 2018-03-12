a2019c6b0da44ed3a7d057ddc32329ba364896cd
cembaykara <babayk@ttu.ee>
2018-03-12 23:12:31 +0200

    add reverse conversion with EUR

--- Class VI/js/requestHandler.js
+++ Class VI/js/requestHandler.js
@@ -9,8 +9,13 @@ $(document).ready(function () {
   const url = "http://data.fixer.io/api/";
   const access_key = "?access_key=ea0166aed8162018a4d24d9e951142a4";
 
-  let rate;
+  console.log($("#picker-to").val())
+
+  var symbols;
   var rates;
+  let rate;
+  let isEur = true;
+  let tmpTo;
   var oldAmount;
 
   //Request currencies and set to picker-to
@@ -18,15 +23,22 @@ $(document).ready(function () {
     url: url + "symbols" + access_key,
     dataType: "jsonp",
     success: function (json) {
-      $.each(json.symbols, function (key1, entry) {
-        $("#picker-to").append($('<option></option>').attr('value', key1).text(key1));
+      symbols = json.symbols
+      $.each(symbols, function (key1, entry) {
+        if (key1 == "EUR") {
+          $("#picker-from").append($('<option></option>').attr('value', key1).text(key1));
+          $("#picker-to").append($('<option></option>').attr('value', key1).text(key1));
+        } else {
+          $("#picker-to").append($('<option></option>').attr('value', key1).text(key1));
+          $("#picker-from").append($('<option disabled = true></option>').attr('value', key1).text(key1));
+        }
       })
     },
     fail: function (xhr, statusText, body) {
       console.log(body);
     }
   });
-//Request currency rates with base=EUR
+  //Request currency rates with base=EUR
   $.ajax({
     url: url + "latest" + access_key,
     dataType: "jsonp",
@@ -36,17 +48,29 @@ $(document).ready(function () {
   });
 
   $("#search-button").click(function () {
-    let symbol = $("#picker-to").val();
-    rate = rates[symbol]
-    if ($("#picker-to").val() != "default" && $.isNumeric($("#textField").val())) {
+    if ($("#picker-to").val() != null && $.isNumeric($("#textField").val()) && isEur) {
+      rate = rates[$("#picker-to").val()];
       $("#resultText").text($("#textField").val() * rate);
-      oldAmount = $("#textField").val();
+    }else if ($("#picker-to").val() != null && $.isNumeric($("#textField").val()) && !isEur){
+      rate = rates[tmpTo];
+      $("#resultText").text($("#textField").val() / rate);
     }
   });
 
   $("#switch-button").click(function () {
-    if ($("#picker-to").val() != "default" && $.isNumeric($("#textField").val())) {
-
+    if ($("#picker-to").val() != null) {
+      let tmp = $("#picker-from").val();
+      if (isEur) {
+        $("#picker-from").val($("#picker-to").val()).change();
+        tmpTo = $("#picker-to").val();
+        $("#picker-to").val(tmp).change();
+        isEur = false;
+      } else {
+        $("#picker-from").val("EUR").change();
+        $("#picker-to").val(tmpTo).change();
+        isEur = true;
+      }
     }
+    $("#search-button").click()
   });
 });
